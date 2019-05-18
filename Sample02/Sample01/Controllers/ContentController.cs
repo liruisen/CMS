cec84b17d2a30d5bcf344333ed40499e36640559
ruisenLi <ruisenLi@163.com>
2019-05-18 17:11:15 +0800

    添加sample01的代码，继续编写sample02的案例

---
+++ Sample02/Sample01/Controllers/ContentController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Sample01.Models;
+
+namespace Sample01.Controllers
+{
+    public class ContentController : Controller
+    {
+        public IActionResult Index()
+        {
+            var contents = new List<Content>();
+            for (int i = 0; i < 10; i++)
+            {
+                contents.Add(new Models.Content()
+                {
+                    id = i,
+                    title = $"{i}的标题",
+                    content = $"{i}的正文",
+                    status = 1,
+                    add_time = DateTime.Now.AddDays(-i),
+                });
+            }
+            return View(new ContentViewModel() { Contents=contents});
+        }
+    }
+}
---
+++ Sample02/Sample01/Models/Content.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Sample01.Models
+{
+    public class Content
+    {
+        /// <summary>
+        /// Id
+        /// </summary>
+        public int id { get; set; }
+        /// <summary>
+        /// 标题
+        /// </summary>
+        public string title { get; set; }
+        /// <summary>
+        /// 内容
+        /// </summary>
+        public string content { get; set; }
+        /// <summary>
+        /// 状态 1正常 0删除
+        /// </summary>
+        public int status { get; set; }
+        /// <summary>
+        /// 添加时间
+        /// </summary>
+        public DateTime add_time { get; set; }
+        /// <summary>
+        /// 修改时间
+        /// </summary>
+        public DateTime modify_time { get; set; }
+    }
+    public class ContentViewModel
+    {
+        public List<Content> Contents { get; set; }
+    }
+}
